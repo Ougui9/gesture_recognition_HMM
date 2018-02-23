e792d281058a389fbf45f5f54e25995463c571b5
Ougui9 <33538250+Ougui9@users.noreply.github.com>
2018-02-22 22:33:46 -0500

    finish EM(learning part)

--- try.py
+++ try.py
@@ -14,6 +14,9 @@ class HMM(n_states=None, n_obs=None, Pi=None, A=None, B=None):
         self.A=A # (N, N)
         self.B=B #(N, M)
 
+        self.Pi_log = np.log(self.Pi)  # (N, 1)
+        self.A_log = np.log(self.A)  # (N, N)
+        self.B_log = np.log(self.B)  # (N, M)
 
     def log_forward(self, obs_sequence):
         T=len(obs_sequence)#obs_sequence: (T, 1)
@@ -24,7 +27,9 @@ class HMM(n_states=None, n_obs=None, Pi=None, A=None, B=None):
         #induc alpha
         for t in range(0,T-1):
             alpha[t+1]=alpha[t].dot(self.A).T*self.B[:, obs_sequence[t+1]]
-
+        self.alpha_log=np.log(alpha)
+        # alpha /= np.sum(alpha, axis=1)
+        pass
 
     def log_backward(self,obs_sequence):
         T = len(obs_sequence)
@@ -35,10 +40,40 @@ class HMM(n_states=None, n_obs=None, Pi=None, A=None, B=None):
         for t in range(T-2,-1,-1):
             beta[t]=self.A.dot((self.B[:,obs_sequence[t+1]]*beta[t+1]).reshape(-1,1))
 
+        self.beta_log=np.log(beta)
+        pass
 
     def baum_welch(self, obs_sequence_list, max_iter=100):
+        T=len(obs_sequence_list)
+        #E-step
+        eps_log=np.zeros([self.N,self.N,T])
+        gamma_log=np.zeros([self.N, T])
+
+        for iter in range(max_iter):
 
 
+
+            # CAL eps
+            for t in range(T-1):
+                eps_log[:,:,t]=self.A_log+self.alpha_log[t]+(self.beta_log[t+1]+self.B_log[:,obs_sequence_list[t+1]]).reshape(-1,1)
+            norm_eps=np.log(np.sum(np.exp(eps_log),axis=2))
+            eps_log-=norm_eps
+            #cal gamma
+            gamma_log=np.log(np.sum(np.exp(eps_log),axis=1))
+
+
+            #M-step
+            gamma=np.exp(gamma_log)
+            eps=np.exp(eps_log)
+            self.Pi_log=gamma_log[:,0]
+            self.A_log=np.log(np.sum(eps[:,:,:-1],axis=2))-np.log(np.sum(gamma[:,:-1],axis=1))
+            for vk in range(np.shape(self.B_log)[1]):
+                self.B_log[:,vk]=np.log(np.sum(gamma[:,obs_sequence_list==vk],axis=1))-np.log(np.sum(gamma,axis=1))
+
+            crit=np.norm(self.A_log)
+
+    def initPara(self):
+        
 
 def data2observation():
 	
